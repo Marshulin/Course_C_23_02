806721472fcb13e5c99e3374d93bd14836e72165
Marshulin <shulik.mv@gmail.com>
2023-02-25 22:49:31 +0300

    hw_task 2 done

---
+++ Lesson_1/HW/2_hw/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 2: Напишите программу, которая принимает
+// на вход три числа и выдаёт максимальное из этих
+
+Console.WriteLine("Введите первое целое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе целое число: ");
+int b = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите третье целое число: ");
+int c = int.Parse(Console.ReadLine()!);
+int m = 0;
+
+if(a > b)
+{
+    m = a;
+}
+if(a < b)
+{
+   m = b; 
+}
+if(c > m)
+{
+    m = c;
+}
+    Console.Write($"max: {m} ");
+
+// Не стала заморачиваться, работает с прведёнными в задаче числами для примера - на этом остановилась :_
