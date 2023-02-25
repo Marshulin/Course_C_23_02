a1180efa677e925bf448eda94efd01b6aac45ffa
Marshulin <shulik.mv@gmail.com>
2023-02-25 21:43:47 +0300

    task 1_2

---
+++ Lesson_1/1_2/Program.cs
@@ -0,0 +1,32 @@
+﻿// дни недели
+int a = int.Parse(Console.ReadLine()!);
+
+
+if(a == 1)
+{
+    Console.WriteLine("Mon");
+}
+else if(a == 2)
+{
+   Console.WriteLine("Tue"); 
+}
+else if(a == 3)
+{
+   Console.WriteLine("Wen"); 
+}
+else if(a == 4)
+{
+   Console.WriteLine("Thu"); 
+}
+else if(a == 5)
+{
+   Console.WriteLine("Fra"); 
+}
+else if(a == 6)
+{
+   Console.WriteLine("Sut"); 
+}
+else if(a == 7)
+{
+   Console.WriteLine("Sun"); 
+}
