4403134ff3c882395d73fe43834b11d882b44cfa
Marshulin <shulik.mv@gmail.com>
2023-02-25 22:30:19 +0300

    hw_task 1 done

---
+++ Lesson_1/HW/1_hw/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 1: Напишите программу, которая на вход
+// принимает два числа и выдаёт, какое число
+// большее, а какое меньшее.
+
+Console.WriteLine("Введите первое целое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе целое число: ");
+int b = int.Parse(Console.ReadLine()!);
+if(a > b)
+{
+    Console.WriteLine("Первое число больше второго.");
+}
+if(a < b)
+{
+    Console.WriteLine("Первое число меньше второго.");
+}
+if(a == b)
+{
+    Console.WriteLine("Введённые числа равны!");
+}
