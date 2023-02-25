084889ccd6c84e15719d21624066fa002483cc44
Marshulin <shulik.mv@gmail.com>
2023-02-25 21:35:05 +0300

    task 1_1

---
+++ Lesson_1/1_1/Program.cs
@@ -0,0 +1,12 @@
+﻿//квадрат ли числа?
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+if(a == b * b)
+{
+    Console.WriteLine("Yes!");
+}
+else
+{
+    Console.WriteLine("No!");
+}
+
