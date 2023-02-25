f287effe5718c2f9cb05d865abfb7d8152fd738b
Marshulin <shulik.mv@gmail.com>
2023-02-25 23:21:54 +0300

    hw_task 4 done

---
+++ Lesson_1/HW/4_hw/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 4: Напишите программу, которая на вход
+// принимает число (N), а на выходе показывает все
+// чётные числа от 1 до N.
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите целое число N: ");
+        int N = int.Parse(Console.ReadLine()!);
+        int b = 2;
+        Console.WriteLine("Чётные числа от 1 до N");
+        while (b <= N)
+        {
+            Console.Write($"{b} ");
+            b = b + 2;
+        }
+    }
+}
+
+// без исключения и предупреждений возможных "вводов" ломающих программку
