79d9efd7d887ebeeca3ff98abd0740c943e27735
Marshulin <shulik.mv@gmail.com>
2023-02-27 20:36:56 +0300

    Task 2_4

---
+++ 2_lesson/2_4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход
+// число и проверяет, кратно ли оно одновременно 7 и
+// 23.
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+void Div(int num)
+{
+    if (num % 7 == 0 & num % 23 ==0)
+        Console.WriteLine("kratno");
+    else 
+        Console.WriteLine("No");
+}
+int a = int.Parse(Console.ReadLine()!);
+
+Div(a);
