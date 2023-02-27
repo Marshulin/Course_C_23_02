974f7ac49c13ed446c9e53a32c00e0c8045fc8d4
Marshulin <shulik.mv@gmail.com>
2023-02-27 21:14:02 +0300

    HW Task 2_2

---
+++ 2_lesson/HW/2_2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 2: Напишите программу, которая выводит
+// случайное трёхзначное число и удаляет вторую цифру
+// этого числа.
+
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int TakeNum(int num)
+{
+    Console.WriteLine(num);
+    int num1 = num / 100;
+    int num3 = num % 10;
+    int xten = num1 * 10;
+    int newnum = xten + num3;
+    
+    return newnum;
+}
+
+int result = TakeNum(new Random().Next(100, 1000));
+Console.WriteLine(result);
