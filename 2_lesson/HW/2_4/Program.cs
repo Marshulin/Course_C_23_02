319a7f62a26b7b4ab3eced8d43b47e1627c40411
Marshulin <shulik.mv@gmail.com>
2023-03-06 15:21:41 +0300

    HW Task 2_1 done

--- 2_lesson/HW/2_4/Program.cs
+++ 2_lesson/HW/2_4/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 4: Напишите программу, которая принимает на
+// вход цифру, обозначающую день недели, и проверяет,
+// является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введите цифру дня недели: ");
+
+void TakeNum(int num)
+{
+    if(num > 5)
+        Console.WriteLine("да");    
+    else
+        Console.WriteLine("нет");
+}
+
+TakeNum(int.Parse(Console.ReadLine()!));
