0d83a7ffd9be88c418d09a4c1ef45e76c6b36e2e
Marshulin <shulik.mv@gmail.com>
2023-03-06 16:44:54 +0300

    Task 3_3

---
+++ 3_lesson/3_3/Program.cs
@@ -0,0 +1,14 @@
+﻿
+void TakeNum(int N)
+{
+    int count = 1;
+    while(count <= N)
+    {
+        Console.WriteLine(Math.Pow(count,2));
+        count ++;
+    }
+
+}
+
+int N = int.Parse(Console.ReadLine()!);
+TakeNum(N);
