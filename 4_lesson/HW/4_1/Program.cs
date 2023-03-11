2f38e2e58743a8396e6c3fa81bbad4d591e11b34
Marshulin <shulik.mv@gmail.com>
2023-03-11 14:34:04 +0300

    HW task 4_1

---
+++ 4_lesson/HW/4_1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+int ExponNum(int A, int B)
+{
+    int expon = 1;
+    for (int i = 1; i <= B; i++)
+        expon *= A;
+
+    return expon;
+}
+
+Console.WriteLine("Введите первое число");
+int A = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Bведите второе число");
+int B = int.Parse(Console.ReadLine()!);
+int result = ExponNum(A, B);
+Console.WriteLine("Число A в натуральной степени B: " + result);
