5f2c5e5ccd77c0bc9f77922d77b47b2ae2d7f95b
Marshulin <shulik.mv@gmail.com>
2023-03-11 14:06:04 +0300

    task 4_3

---
+++ 4_lesson/4_3/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+void Massiv(int mas)
+{
+    int[] numbers = new int[mas];
+    for (int i = 0; i < mas; i++)
+    {
+        numbers[i] = new Random().Next(2);
+        Console.WriteLine(numbers[i]);
+    }
+}
+
+Massiv(8);
