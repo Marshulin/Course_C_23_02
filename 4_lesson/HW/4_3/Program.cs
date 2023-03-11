d9de720b0b26120087582d099650219dada65de6
Marshulin <shulik.mv@gmail.com>
2023-03-11 15:56:08 +0300

    HW task 4_3

---
+++ 4_lesson/HW/4_3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+void Massiv(int mas, int min, int max)
+{
+    int[] numbers = new int[mas];
+    for (int i = 0; i < mas; i++)
+    {
+        numbers[i] = new Random().Next(min, max);
+        Console.Write(numbers[i] + ",");
+    }
+}
+
+Console.WriteLine("Введите число элементов массива");
+int mas = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите минимальное число");
+int min = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите максимальное число");
+int max = int.Parse(Console.ReadLine()!);
+Massiv(mas, min, max );
+
