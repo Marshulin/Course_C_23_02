a79303ca3e6815d49ff316e45fe9c731cd2ce3ef
Marshulin <shulik.mv@gmail.com>
2023-03-11 13:48:20 +0300

    task 4_2

---
+++ 4_lesson/4_2/Program.cs
@@ -0,0 +1,13 @@
+﻿
+int CompNum(int num)
+{
+    int all_comp = 1;
+    for (int i = 1; i <= num; i++)
+        all_comp *= i;
+
+    return all_comp;
+}
+
+int A = int.Parse(Console.ReadLine()!);
+int result = CompNum(A);
+Console.WriteLine(result);
