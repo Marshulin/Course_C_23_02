d969686f62dcc5fc0d04578d352f95b7a721e018
Marshulin <shulik.mv@gmail.com>
2023-03-12 10:49:26 +0300

    HW task 5_1

---
+++ 5_lesson/HW/5_1/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+        Console.Write($" {arr[i]} ");
+    Console.WriteLine();
+}
+
+int[] MassNums(int size)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(100, 1000);
+    return arr;
+}
+
+int FlatNums(int[] mass)
+{
+    int count = 0;
+    int Flat = 0;
+    for (int i = 0; i < mass.Length; i++)
+    {
+        if (mass[i] % 2 == 0)
+        Flat++;
+        count++;
+    }
+    return Flat;
+}
+
+Console.WriteLine("Введите размер массива");
+int num = int.Parse(Console.ReadLine()!);
+
+int[] mass = MassNums(num);
+Print(mass);
+
+Console.WriteLine("Количество чётных в массиве: " + FlatNums(mass));
