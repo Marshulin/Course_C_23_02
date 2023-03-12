7633f2c6768b39d8fae9e548e35df601c3beef64
Marshulin <shulik.mv@gmail.com>
2023-03-12 12:02:17 +0300

    HW task 5_2

---
+++ 5_lesson/HW/5_2/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+int[] MassNums(int size, int from, int to)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(from, to);
+    return arr;
+}
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+        Console.Write($" {arr[i]} ");
+    Console.WriteLine();
+}
+
+int SumFlatNums(int[] mass)
+{
+    int sumFlat = 0;
+    for (int i = 0; i < mass.Length; i = i + 2)
+        sumFlat += mass[i];
+        return sumFlat;
+}
+
+Console.WriteLine("Введите размер массива");
+int num = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите начало");
+int start = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите окончание");
+int stop = int.Parse(Console.ReadLine()!);
+
+int[] mass = MassNums(num, start, stop);
+Print(mass);
+
+Console.WriteLine("->" + SumFlatNums(mass));
