31f3ae5f2a40b92e20acc89ad9bd3077d6208d82
Marshulin <shulik.mv@gmail.com>
2023-03-24 17:20:56 +0300

    Task 6_0, 6_10

---
+++ 6_lesson/6_1/Program.cs
@@ -0,0 +1,20 @@
+﻿
+void Triangle (int A, int B, int C)
+	
+	{
+	    if (A < B + C & B < A + C & C < A + B)
+	    Console.WriteLine("yes");
+	
+	    else
+	    Console.WriteLine("no");
+	
+	
+	}
+	
+	
+	int N1 =int.Parse(Console.ReadLine()!);
+	int N2 =int.Parse(Console.ReadLine()!);
+	int N3 =int.Parse(Console.ReadLine()!);
+	
+	
+	Triangle(N1, N2, N3);
