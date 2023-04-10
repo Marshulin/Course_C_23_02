89ff26f1841e304973095ac676a6a0235b7c96c2
Marshulin <shulik.mv@gmail.com>
2023-04-10 21:35:43 +0300

    HW Task 9_1

---
+++ 9_lesson/HW/9_2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 2: Задайте значения M и N. Напишите программу, которая найдёт сумму
+// натуральных элементов в промежутке от M до N с помощью рекурсии.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8 -> 30
+
+void NumsFromTo(int M, int N)
+{
+    if(M == N + 1) return;
+    NumsFromTo(M, N - 1);
+    Console.Write($"{N}, ");
+
+}
+
+Console.WriteLine("M: ");
+int m = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("N: ");
+int n = int.Parse(Console.ReadLine()!);
+
+NumsFromTo(m, n);
