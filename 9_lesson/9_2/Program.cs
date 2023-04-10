e52f401ac4ac3bca24dc7dcffb992d312b0aff50
Marshulin <shulik.mv@gmail.com>
2023-04-10 21:06:24 +0300

    lesson 9

---
+++ 9_lesson/9_2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 2: Задайте значения M и N. Напишите программу, которая
+// рекурсивно выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 6, 7, 8"
+
+void NumsFromTo(int M, int N)
+{
+    if(M == N + 1) return;
+    NumsFromTo(M, N - 1);
+    Console.Write($"{N}, ");
+
+}
+
+Console.WriteLine("M: ");
+int m = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("N: ");
+int n = int.Parse(Console.ReadLine()!);
+
+NumsFromTo(m, n);
---
+++ 9_lesson/9_3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 3: Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр. Использовать рекурсию.
+// 453 -> 12
+// 45 -> 9
+
+int SumDigits(int N)
+{
+    if (N == 0) return 0;
+    return SumDigits(N / 10) + N % 10;
+}
+
+Console.WriteLine("N: ");
+int n = int.Parse(Console.ReadLine()!);
+
+Console.Write(SumDigits(n));
